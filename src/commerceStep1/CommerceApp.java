f32a3eef7250c4f4603cb95c376dee29e378a27d
jihyeon <yio1346@gmail.com>
2025-12-15 11:55:04 +0900

    상품 리스트화로 출력 구현

--- src/commerceStep1/CommerceApp.java
+++ src/commerceStep1/CommerceApp.java
@@ -1,9 +1,22 @@
 package commerceStep1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CommerceApp
 {
     public static void main(String[] args)
     {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 10));
+        products.add(new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 10));
+        products.add(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 5));
+        products.add(new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 20));
 
+        for (int i = 0; i < products.size(); i++)
+        {
+            System.out.println((i + 1) + ". " + products.get(i));
+        }
     }
 }
--- src/commerceStep1/Product.java
+++ src/commerceStep1/Product.java
@@ -2,5 +2,22 @@ package commerceStep1;
 
 public class Product
 {
+    private String name;
+    private int price;
+    private String description;
+    private int stock;
 
+    public  Product(String name, int price, String description, int stock)
+    {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.stock = stock;
+    }
+
+
+    public String toString()
+    {
+        return name + " | " + String.format("%,d", price) + "원 | " + description;
+    }
 }
