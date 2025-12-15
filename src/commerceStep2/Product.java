ff6962033c1fdcf06968ed40af05ce319c9a75e3
jihyeon <yio1346@gmail.com>
2025-12-15 12:24:52 +0900

    CommerceSystem으로 로직 이동

---
+++ src/commerceStep2/Product.java
@@ -0,0 +1,28 @@
+package commerceStep2;
+
+public class Product
+{
+    private String name;
+    private int price;
+    private String description;
+    private int stock;
+
+    public  Product(String name, int price, String description, int stock)
+    {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.stock = stock;
+    }
+
+    public String  getName()
+    {
+        return this.name;
+    }
+
+    public String toString()
+    {
+        return name + " | " + String.format("%,d", price) + "원 | " + description;
+    }
+
+}
